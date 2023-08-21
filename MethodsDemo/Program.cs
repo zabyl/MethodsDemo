f3a8793ceae11065fdb1a1f574b4002c21477a42
UgrasK <ugurkaraer1989@gmail.com>
2023-08-22 01:04:12 +0300

    Add project files.

---
+++ MethodsDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int average = Average(9, 36, 18);
+            
+            Methods print = new Methods();
+            print.PrintToConsole(Convert.ToString(average));
+
+            Console.ReadLine();
+            
+        }
+
+        static int Average(int num1, int num2, int num3)
+        {
+            return (num1 + num2 + num3) / 3;
+        }
+
+    }
+
+    class Methods
+    {
+        public void PrintToConsole(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
